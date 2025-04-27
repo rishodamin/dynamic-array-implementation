fd4f4d7a4e21bdfd69b65ec99226ee339dd90e1f
Damin Risho <daminrisho@gmail.com>
2025-04-27 12:07:32 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INITIAL_CAPACITY 8
+
+typedef struct {
+    size_t size;
+    size_t capacity;
+    int* array;
+} dynamic_array;
+
+dynamic_array* array() {
+    dynamic_array* arr = (dynamic_array*) malloc(sizeof(dynamic_array));
+    if (!arr){
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    arr->size = 0;
+    arr->capacity = INITIAL_CAPACITY;
+    arr->array = (int*)malloc(INITIAL_CAPACITY*sizeof(int));
+    if (!arr->array){
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    return arr;
+}
+
+void insertArray(dynamic_array* arr, int num) {
+    if (arr->size==arr->capacity){
+        int* temp = arr->array;
+        arr->capacity <<= 1;
+        arr->array = realloc(arr->array, arr->capacity * sizeof(int));
+        arr->array = temp;
+    }
+    arr->array[arr->size++] = num;
+}
+
+void deleteArray(dynamic_array* arr, int index) {
+    if (index >= arr->size){
+        printf("Index out of range\n");
+        return;
+    }
+    for (int i=index; i<arr->size-1; ++i){
+        arr->array[i] = arr->array[i+1];
+    }
+    arr->size--;
+}
+
+void displayArray(dynamic_array* arr) {
+    for (int i=0; i<arr->size; ++i){
+        printf("%d ", arr->array[i]);
+    }
+    printf("\n");
+}
+
+
+void freeArray(dynamic_array* arr){
+    free(arr->array);
+    free(arr);
+}
+
+int main(){
+    dynamic_array* arr = array();
+
+    insertArray(arr, 5);
+    insertArray(arr, 3);
+    insertArray(arr, 2);
+
+    displayArray(arr);
+
+    deleteArray(arr, 1);
+
+    displayArray(arr);
+
+    freeArray(arr);
+    return 0;
+}
